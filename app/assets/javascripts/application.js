68aefed7ffc1a708959e5191cc955ee99a447c9b
Matt Stjernholm <thew88@gmail.com>
2016-01-11 14:57:41 -0700

    add animation

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,48 @@
 //= require turbolinks
 //= require semantic_ui/semantic_ui
 //= require_tree .
+
+$( document ).ready(function() {
+
+  $('.ui.dropdown')
+  .dropdown();
+
+  $('.login-form, .home-btn, .sub-header, .login-text, .new-member').hide();
+
+  $('.login-form, .login-text')
+    .transition({
+      animation : 'fade up',
+      duration  : 800,
+      interval  : 400
+    })
+  ;
+
+  var delay=250;
+  setTimeout(function(){
+    $('.sub-header')
+    .transition({
+      animation: 'scale',
+      duration: 800});
+  }, delay);
+
+  var delay=500;
+  setTimeout(function(){
+    $('.home-btn')
+    .transition({
+      animation: 'scale',
+      duration: 1000}).transition({
+        animation: 'bounce',
+        duration: 1000});;
+  }, delay);
+
+
+  var delay=2000;
+  setTimeout(function(){
+    $('.new-member')
+    .transition({
+      animation: 'bounce',
+      duration: 1000});
+  }, delay);
+
+
+});
