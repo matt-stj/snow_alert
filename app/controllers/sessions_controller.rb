4fdc0591182ab0e8b2371587fcb226b5a1357b16
Matt Stjernholm <thew88@gmail.com>
2016-01-11 21:49:10 -0700

    user dash mocked up

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,15 @@ class SessionsController < ApplicationController
   end
 
   def create
-    binding.pry
+     if user = User.from_omniauth(request.env["omniauth.auth"])
+       session[:user_id] = user.id
+     end
+     redirect_to root_path
+  end
+
+  def destroy
+    session.clear
+    flash[:notice] = "Thanks for visiting. You've been logged out."
     redirect_to root_path
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,17 @@ Rails.application.routes.draw do
 
 root to: 'home#index'
 
-# get '/login', to: 'sessions#new'
+get '/create_account', to: 'sessions#new'
 # post '/login', to: 'sessions#create'
 delete '/logout', to: 'sessions#destroy'
 
 get '/auth/facebook/callback' => 'sessions#create'
 get '/auth/facebook', as: 'login'
 
-resources :users, only: [:show]
+resources :users, only: [:show, :edit]
+
+get '/resort-example', to: 'resorts#example'
+resources :resorts, only: [:index, :show]
 
 
 
