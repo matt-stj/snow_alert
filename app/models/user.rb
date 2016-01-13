c5b35cc9615c28420615c1dd2add24bf6309db55
Matt Stjernholm <thew88@gmail.com>
2016-01-12 17:35:26 -0700

    add snowfall amount to users table

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ActiveRecord::Base
       new_user.uid                = auth_info.uid
       new_user.name               = auth_info.info.name
       new_user.screen_name        = auth_info.extra.raw_info.id
-      new_user.image_url          = auth_info.info.image
+      new_user.image_url          = "https://graph.facebook.com/#{auth_info.uid}/picture?type=large"
       new_user.oauth_token        = auth_info.credentials.token
     end
   end
