86ccb4ff6c337fd84931fbd3fa1579a22884aee8
Matt Stjernholm <thew88@gmail.com>
2016-01-14 13:53:14 -0700

    refactor caching in mountains-show page to app helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+
+  def cache_key_for_mountain(mountain)
+    mountain_name = mountain.name.parameterize
+    time_stamp = Time.now.strftime("%Y-%m-%e-%H")
+    [mountain_name, "data", time_stamp].join("-")
+  end
+
 end
