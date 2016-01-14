0e057a99ae35aae5e570c84c80c0def3edbbe96f
Matt Stjernholm <thew88@gmail.com>
2016-01-14 16:51:30 -0700

    add closest mountain lookup method

--- app/models/mountain.rb
+++ app/models/mountain.rb
@@ -2,7 +2,14 @@ class Mountain < ActiveRecord::Base
   has_many :favorites
   has_many :users, through: :favorites
 
-  def update_user
+  def closest_mountain(latitude, longitude)
+    trunc_latitude = truncate_coordinate(latitude)
+    truncate_longitude = truncate_coordinate(longitude)
+    Mountain.where("latitude LIKE :latitude", latitude: "%#{trunc_latitude}%").where("longitude LIKE :longitude", longitude: "%#{truncate_longitude}%")
+  end
 
+  def truncate_coordinate(coordinate)
+    string = sprintf "%.3f", coordinate
+    string[0..(string.length - 2)]
   end
 end
